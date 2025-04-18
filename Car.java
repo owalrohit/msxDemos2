6e3be4d55b46c651254f50c23b8ba206b512c61f
Rohit Owal <rohitowal@Mindstixs-MacBook-Pro-5.local>
2025-04-18 15:38:20 +0530

    add color method in car class

--- Car.java
+++ Car.java
@@ -1,5 +1,13 @@
 public class Car{
 	public static void main(String args[]){
 		System.out.println("inside car class");
+	
+
+	Car c = new Car();
+	c.color();
+
+	public void color(){
+		Sysetm.out.print("red");
 	}
+}
 }
